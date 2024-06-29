8237a7cb66a80064d834e9fa33903c7827f8b209
Sai Charan Narla <69081850+cherri18@users.noreply.github.com>
2024-06-29 12:28:20 +0530

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first sample CD project'));
+app.get('/', (req, res) => res.send('This is my first sample CD project and now the updated message'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
